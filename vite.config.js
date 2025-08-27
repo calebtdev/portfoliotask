0c5dcee79f24da0e895bc6bcd03daf4da29c0fbb
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-08-27 12:34:38 -0700

    update on the portfolio task

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  // Important for React Router SPA routing on Vercel
+  base: "/",
+});
